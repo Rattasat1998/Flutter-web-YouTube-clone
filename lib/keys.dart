632f59fb3240afc0bc6569f1ed3787176a4c35af
Rattasat Phabsing <46988673+Rattasat1998@users.noreply.github.com>
2023-06-15 22:15:19 +0700

    junior
    part 2

--- lib/keys.dart
+++ lib/keys.dart
@@ -1,3 +1,3 @@
 class Keys {
-  static  const youtubeKey = 'AIzaSyCX3X9XKdIaYajbANRv3xv432bkh7vj_Rc';
+  static  const youtubeKey = 'AIzaSyD2TnjUNQr9E4aHDUpT-B6hajavxzjHGv4';
 }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:youtube_api/youtube_api.dart';
 import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
-import 'models/video_data_model.dart';
+import '../../keys.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,10 +13,77 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final YoutubeAPI _youtubeAPI = YoutubeAPI(Keys.youtubeKey); // key from google api
+
+  bool loading = false;
+
+  List<YouTubeVideo> result = [];
+  String _query = 'flutter bloc';
+
+  getVideoFromYouTube() async {
+    try {result =
+    await _youtubeAPI.search(_query, order: 'relevance', videoDuration: 'any', type: 'video');
+    result = await _youtubeAPI.nextPage();} catch (e){
+      print(e);
+    }
+    setState(() {
+      loading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    loading = true;
+    super.initState();
+    getVideoFromYouTube();
+  }
+
   @override
   Widget build(BuildContext context) {
-
-    List<String> tags = ['one', 'two','Grey','Green','Color','Menu','Food','Asp', 'two','Grey','Green','Color','Menu','Food','Asp', 'two','Grey','Green','Color','Menu','Food','Asp', 'two','Grey','Green','Color','Menu','Food','Asp','Grey','Green','Color','Menu','Food','Asp', 'two','Grey','Green','Color','Menu','Food','Asp'];
+    List<String> tags = [
+      'one',
+      'two',
+      'Grey',
+      'Green',
+      'Color',
+      'Menu',
+      'Food',
+      'Asp',
+      'two',
+      'Grey',
+      'Green',
+      'Color',
+      'Menu',
+      'Food',
+      'Asp',
+      'two',
+      'Grey',
+      'Green',
+      'Color',
+      'Menu',
+      'Food',
+      'Asp',
+      'two',
+      'Grey',
+      'Green',
+      'Color',
+      'Menu',
+      'Food',
+      'Asp',
+      'Grey',
+      'Green',
+      'Color',
+      'Menu',
+      'Food',
+      'Asp',
+      'two',
+      'Grey',
+      'Green',
+      'Color',
+      'Menu',
+      'Food',
+      'Asp',
+    ];
 
     return Scaffold(
       backgroundColor: Colors.grey.shade900,
@@ -27,79 +95,126 @@ class _HomeScreenState extends State<HomeScreen> {
               flex: 5,
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    _buildTags(tags),
+                child: loading
+                    ? CupertinoActivityIndicator()
+                    : Column(
+                        children: [
+                          _buildTags(tags),
+                          // TODO get data from api
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: GridView.builder(
+                                  itemCount: result.length,
+                                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 3,
+                                    crossAxisSpacing: 10,
+                                    mainAxisSpacing: 10,
+                                    childAspectRatio: 1.5,
+                                  ),
+                                  itemBuilder: (context, index) {
+                                    YouTubeVideo item = result[index];
 
-                    /// Part2
-
-                  ],
-                ),
-              )
-          )
+                                    return GestureDetector(
+                                      onTap: (){
+                                        print('watch');
+                                      },
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          color: Colors.white,
+                                            borderRadius: BorderRadius.circular(8)),
+                                        child: Column(
+                                          children: [
+                                            SizedBox(
+                                                height: 200,
+                                                width: MediaQuery.of(context).size.width,
+                                                child: YoutubePlayer(controller: YoutubePlayerController.fromVideoId(
+                                                    videoId: extractVideoId(
+                                                      item.url.toString(),
+                                                    )))),
+                                            Text(item.title, style: Theme.of(context).textTheme.headline6),
+                                          ],
+                                        ),
+                                      ),
+                                    );
+                                  }),
+                            ),
+                          ),
+                        ],
+                      ),
+              ))
         ],
       ),
     );
   }
 
-  SingleChildScrollView _buildTags(List<String> tags) {
+  String extractVideoId(String url) {
+    RegExp regExp = RegExp(
+      r'(?:youtu\.be/|youtube\.com/(?:embed/|v/|watch\?v=|watch\?.+&v=))([^&?/ ]{11})',
+    );
+    final match = regExp.firstMatch(url);
+    if (match != null && match.groupCount >= 1) {
+      return match.group(1)!;
+    } else {
+      throw Exception('Invalid YouTube video URL');
+    }
+  }
+
+  _buildTags(List<String> tags) {
     return SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    primary: true,
-                    child: Row(
-                      children: [
-                        ...tags.map((e) => Container(
-                            margin: EdgeInsets.only(right: 10),
-                            decoration: BoxDecoration(
-                              color: Colors.white30,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            padding: EdgeInsets.symmetric(horizontal: 16,vertical: 10),
-                            child: Text(e,style: TextStyle(color: Colors.white)))).toList()
-                      ],
-                    ),
-                  );
+      scrollDirection: Axis.horizontal,
+      primary: true,
+      child: Row(
+        children: tags
+            .map((e) => Container(
+                margin: const EdgeInsets.only(right: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white30,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                child: Text(e, style: const TextStyle(color: Colors.white))))
+            .toList(),
+      ),
+    );
   }
 
   Expanded _buildLeftMenu(BuildContext context) {
     return Expanded(
-            flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  _buildButtonMenu(
-                      context: context,
-                      title: 'Home',
-                      icon: Icons.home,
-                      isSelected: true),
-                  _buildButtonMenu(
-                      context: context,
-                      title: 'Shorts',
-                      icon: Icons.video_library_outlined),
-                  _buildButtonMenu(
-                      context: context,
-                      title: 'Subscriptions',
-                      icon: Icons.subscriptions_outlined),
-                  _buildButtonMenu(
-                      context: context,
-                      title: 'Originals',
-                      icon: Icons.video_library_outlined),
-                  _buildButtonMenu(context: context, title: 'YouTube Music', icon: Icons.library_music_outlined),
-                  Divider(thickness: .5, color: Colors.grey),
-                  _buildButtonMenu(context: context, title: 'Library', icon: Icons.library_books_rounded),
-                  _buildButtonMenu(context: context, title: 'History', icon: Icons.history),
-                  _buildButtonMenu(context: context, title: 'Your video', icon: Icons.ondemand_video),
-                  _buildButtonMenu(context: context, title: 'Watch later', icon: Icons.watch_later_outlined),
-                  _buildButtonMenu(context: context, title: 'Downloads', icon: Icons.download),
-                  _buildButtonMenu(context: context, title: 'Your Clips', icon: Icons.video_collection),
-                  _buildButtonMenu(context: context, title: 'Show More', icon: Icons.keyboard_arrow_down),
-                  Divider(thickness: .5, color: Colors.grey),
-                  Text('Subscriptions',style: TextStyle(fontSize: 20,color: Colors.white),),
-                ],
+        flex: 1,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildButtonMenu(context: context, title: 'Home', icon: Icons.home, isSelected: true),
+              _buildButtonMenu(
+                  context: context, title: 'Shorts', icon: Icons.video_library_outlined),
+              _buildButtonMenu(
+                  context: context, title: 'Subscriptions', icon: Icons.subscriptions_outlined),
+              _buildButtonMenu(
+                  context: context, title: 'Originals', icon: Icons.video_library_outlined),
+              _buildButtonMenu(
+                  context: context, title: 'YouTube Music', icon: Icons.library_music_outlined),
+              const Divider(thickness: .5, color: Colors.grey),
+              _buildButtonMenu(
+                  context: context, title: 'Library', icon: Icons.library_books_rounded),
+              _buildButtonMenu(context: context, title: 'History', icon: Icons.history),
+              _buildButtonMenu(context: context, title: 'Your video', icon: Icons.ondemand_video),
+              _buildButtonMenu(
+                  context: context, title: 'Watch later', icon: Icons.watch_later_outlined),
+              _buildButtonMenu(context: context, title: 'Downloads', icon: Icons.download),
+              _buildButtonMenu(context: context, title: 'Your Clips', icon: Icons.video_collection),
+              _buildButtonMenu(
+                  context: context, title: 'Show More', icon: Icons.keyboard_arrow_down),
+              const Divider(thickness: .5, color: Colors.grey),
+              const Text(
+                'Subscriptions',
+                style: TextStyle(fontSize: 20, color: Colors.white),
               ),
-            ));
+            ],
+          ),
+        ));
   }
 
   _buildButtonMenu({
@@ -122,10 +237,10 @@ class _HomeScreenState extends State<HomeScreen> {
             icon,
             color: Colors.white,
           ),
-          SizedBox(width: 10),
+          const SizedBox(width: 10),
           Text(
             title,
-            style: TextStyle(color: Colors.white),
+            style: const TextStyle(color: Colors.white),
           )
         ],
       ),
